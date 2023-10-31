f6cac15c31f0c6826d1133e2e1d0b381064836c5
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2023-10-31 11:13:56 +0530

    pushed changes

---
+++ build/app/generated/source/buildConfig/debug/com/cloudyml/cloudymlapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cloudyml.cloudymlapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cloudyml.cloudymlapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 52;
+  public static final String VERSION_NAME = "17.0.0";
+}
